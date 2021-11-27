06b92581edd61add371e6ee34b5c16929745e6b5
amarijano <tonimarijanovic@gmail.com>
2021-11-27 18:28:00 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">Timer</header>
     </div>
   );
 }
