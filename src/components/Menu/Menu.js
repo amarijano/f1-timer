8b143d06e618b3e1b49ac64dc2de20cf8e3d7cc4
amarijano <tonimarijanovic@gmail.com>
2021-12-09 16:02:05 +0100

    nav test

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,20 @@
 import "./menu.scss";
-const Menu = () => {
-  return <div className="main-menu"></div>;
-};
+const Menu = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Contact us",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Sign up",
+    url: "#",
+    cName: "nav-links-mobile",
+  },
+];
 
 export default Menu;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,32 @@
 import "./navbar.scss";
 import { useSize } from "../../hooks";
+import { Menu } from "../Menu";
+import {
+  FieldTimeOutlined,
+  CloseOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+import { useState } from "react";
+import { Button } from "../Button/Button";
+import React, { Component } from "react";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => {
+    setClick(!click);
+  };
   const width = useSize();
   const handleLogin = () => {};
   return (
-    <nav className="navbar">
-      <h1>F1 Timer</h1>
-      <div className="links">
+    <nav className="NavbarItems">
+      <h1 className="navbar-logo">
+        F1 Timer
+        <i className="timer-logo">
+          <FieldTimeOutlined />
+        </i>
+      </h1>
+
+      {/* <div className="links">
         {width > 762 ? (
           <>
             <a href="/login" onClick={handleLogin}>
@@ -20,7 +39,28 @@ const Navbar = () => {
             <p>manj</p>
           </>
         )}
+      </div> */}
+      <div className="menu-icon" onClick={handleClick}>
+        <i>
+          {click ? (
+            <CloseOutlined className="menu-close" />
+          ) : (
+            <MenuOutlined className="menu-open" />
+          )}
+        </i>
       </div>
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        {Menu.map((item, index) => {
+          return (
+            <li key={index}>
+              <a className={item.cName} href={item.url}>
+                {item.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <Button>Sign up</Button>
     </nav>
   );
 };
